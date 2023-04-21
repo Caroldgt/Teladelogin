98dc185fb6ae7cf4107333df936c0782e3041c6d
Carol <caroldaga2@gmail.com>
2023-04-21 19:19:02 -0300

    adicionando estilos de textos e posição

--- Tela-Login/src/App.jsx
+++ Tela-Login/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import Banner from './Componentes/Banner';
 import Textos from './Componentes/Textos';
 
+
 function App() {
 
   return (
     <div>
+    <Textos/>
    <Banner/>
-   <Textos/>
    </div>
   )
 }
--- Tela-Login/src/Componentes/Textos/index.jsx
+++ Tela-Login/src/Componentes/Textos/index.jsx
@@ -1,14 +1,12 @@
-import './textos.css'
+import "./textos.css";
 
-
-
-function Textos (){
-    return(
-        <div className='textos'>
-            <h2>Faça login</h2>
-            <h3>para entrar para o nosso time</h3>
-        </div>
-    )
+function Textos() {
+  return (
+    <div className="textos">
+      <h2>Faça login</h2>
+      <h3>para entrar para o nosso time</h3>
+    </div>
+  );
 }
 
-export default Textos
+export default Textos;
