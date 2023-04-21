af72cbd8de12d7260e4cfe3c7033bdd4e176d531
Carol <caroldaga2@gmail.com>
2023-04-21 01:21:08 -0300

    adicionando estilos aos textos

--- Tela-Login/src/Componentes/Textos/index.jsx
+++ Tela-Login/src/Componentes/Textos/index.jsx
@@ -1,10 +1,10 @@
-import '../Textos'
+import './textos.css'
 
 
 
 function Textos (){
     return(
-        <div>
+        <div className='textos'>
             <h2>Faça login</h2>
             <h3>para entrar para o nosso time</h3>
         </div>
