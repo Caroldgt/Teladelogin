1d248ed7a224c4b7e56195338c735d3dba606dc2
Carol <caroldaga2@gmail.com>
2023-04-21 23:27:37 -0300

    estilizando form

--- Tela-Login/src/Componentes/Formulario/index.jsx
+++ Tela-Login/src/Componentes/Formulario/index.jsx
@@ -1,30 +1,44 @@
-import './formulario.css'
+import { useState } from "react";
 
-
+import "./formulario.css";
 
 function Formulario() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-    const enviofeito = (event) =>{
-        event.preventDefault()
-        console.log("aconteceu")
-    }
+  const enviofeito = (event) => {
+    event.preventDefault();
+    console.log("aconteceu");
+  };
 
-return(
+  return (
     <div>
-        <form className='form' onSubmit={enviofeito}> 
-            <h1>LOGIN</h1>
-            <p>usuario</p>
-            <input type="text" placeholder="Usuario" required/>
-            <p>senha</p>
-            <input type="password" placeholder="qual sua sennha?" required/>
-            <button type="submit">Login</button>
-        </form>
+      <form className="form" onSubmit={enviofeito}>
+        <h1>LOGIN</h1>
+        <label htmlFor="usuario">usuario</label>
+        <input
+        id="usuario"
+          type="text"
+          placeholder="Usuario"
+          required
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <div>
+        <label for="senha">senha</label>
+        <input
+        id="senha"
+          type="password"
+          placeholder="qual sua sennha?"
+          required
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
-)
-
-
-
+  );
 }
 
-
-export default Formulario
+export default Formulario;
