d85d2021c3c99a9f03e6f3cd9c5faf564215312a
Carol <caroldaga2@gmail.com>
2023-04-23 01:59:56 -0300

    textos no responsivo

--- Tela-Login/src/Componentes/Textos/index.jsx
+++ Tela-Login/src/Componentes/Textos/index.jsx
@@ -5,6 +5,10 @@ function Textos() {
     <div className="textos">
       <h2>Faça login</h2>
       <h3>para entrar para o nosso time</h3>
+      <div>
+      <footer><h2>Faça login</h2>
+      <h3>para entrar para o nosso time</h3></footer>
+      </div>
     </div>
   );
 }
